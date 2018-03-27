90ded4740adbf4407ebbc637b05845dacaa189ae
saranyamadhavan <37831109+saranyamadhavan@users.noreply.github.com>
2018-03-27 16:02:16 +0530

    Update positiveornegative.java

--- Beginner level/positiveornegative.java
+++ Beginner level/positiveornegative.java
@@ -1,4 +1,14 @@
+/* package whatever; // don't place package name! */
+
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
 {
+	public static void main (String[] args) throws java.lang.Exception
+	{
 		// your code goes here
 		Scanner s=new Scanner(System.in);
 		int n;
@@ -21,9 +31,10 @@
 			
 			
 		}
-	else
-	{
-		System.out.println("More than 1 lakh");
+		else
+		{
+			System.out.println("More than 1 lakh");
+		}
 		
 	}
 }

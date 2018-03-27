a21f7674a8f5eb55015ad90cc0c786f6790523de
saranyamadhavan <37831109+saranyamadhavan@users.noreply.github.com>
2018-03-27 16:12:42 +0530

    Create oddoreven.java

---
+++ Beginner level/oddoreven.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+import java.util.Scanner;
+ 
+class Ideone
+{
+   public static void main(String args[])
+   {
+      int x;
+      System.out.println("Enter an integer to check if it is odd or even ");
+      Scanner in = new Scanner(System.in);
+      x = in.nextInt();
+ 
+      if ( x % 2 == 0 )
+         System.out.println("You entered an even number.");
+      else
+         System.out.println("You entered an odd number.");
+   }
+}

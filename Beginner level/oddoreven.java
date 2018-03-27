c60b4e021c8d458b8c515d79b608c69abe3ad790
saranyamadhavan <37831109+saranyamadhavan@users.noreply.github.com>
2018-03-27 16:16:07 +0530

    Update oddoreven.java

--- Beginner level/oddoreven.java
+++ Beginner level/oddoreven.java
@@ -9,10 +9,17 @@ class Ideone
       System.out.println("Enter an integer to check if it is odd or even ");
       Scanner in = new Scanner(System.in);
       x = in.nextInt();
+    if(x<100000)
+    {
  
       if ( x % 2 == 0 )
          System.out.println("You entered an even number.");
       else
          System.out.println("You entered an odd number.");
+    }
+   else
+   {
+    System.out.println("More than 1 lakh");
    }
+}
 }

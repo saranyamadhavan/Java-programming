bfde019bb3275341fdf74f6bacf4057f45ebe8a1
saranyamadhavan <37831109+saranyamadhavan@users.noreply.github.com>
2018-03-27 16:26:23 +0530

    Create vowelorconsonant.java

---
+++ Beginner level/vowelorconsonant.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes 
+		int i=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a character : ");
+	char ch=sc.next( ).charAt(0);	 
+	//char ch=sc.nextChar();
+	switch(ch)
+	{
+	 case  'a' :
+	 case 'e'  :
+	 case 'i'   :
+	 case 'o'  :
+	 case 'u'  :
+	 case  'A' :
+	 case 'E'  :
+	 case 'I'   :
+	 case 'O'  :
+	 case 'U'  :i++;
+	}
+	if(i==1)
+	System.out.println("Entered character "+ch+" is  Vowel");
+	else 
+		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Entered character "+ch+" is Consonent");
+		else
+		System.out.println("Not an alphabet");		
+ 
+	}
+}
